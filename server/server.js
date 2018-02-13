91ad9ee5e79434f17633513cfc29a8ff9ae05369
vyshiva <elena_haponyuk@ukr.net>
2018-02-13 20:05:25 +0200

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+var {ObjectID} = require('mongodb');
 var express = require('express');
 var bodyParser = require('body-parser');
 
@@ -26,6 +27,22 @@ app.get('/todos', (req, res) => {
     res.send({todos});
   }, (e) => {
     res.status(400).send(e);
+  });
+});
+
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
+
+  if (!ObjectID.isValid(id)){
+    return res.status(404).send();
+  }
+  Todo.findById(id).then((todo) => {
+    if (!todo){
+      return res.status(404).send();
+    }
+    return res.send({todo});
+  }).catch((e) => {
+    return res.status(400).send();
   });
 });
 
