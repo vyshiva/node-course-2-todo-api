d5de1c6f30f38aac32de67b9693644201e9ebfa7
vyshiva <elena_haponyuk@ukr.net>
2018-02-23 22:36:59 +0200

    Add POST /users/login route

--- server/server.js
+++ server/server.js
@@ -108,6 +108,18 @@ app.post('/users', (req, res) => {
   });
 });
 
+app.post('/users/login', (req, res) => {
+  var body = _.pick(req.body, ['email', 'password']);
+
+  User.findByCredencials(body.email, body.password).then((user) => {
+    return user.generateAuthToken().then((token) => {
+      res.header('x-auth', token).send(user);
+    });
+  }).catch((e) => {
+    res.status(400).send();
+  });
+});
+
 app.get('/users/me', authenticate, (req, res) => {
   res.send(req.user);
 });
