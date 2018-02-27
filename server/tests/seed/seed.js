9b93961070ed810dd9a0347fb2669b3f626206a5
vyshiva <elena_haponyuk@ukr.net>
2018-02-27 19:36:21 +0200

    Associate users with todos

--- server/tests/seed/seed.js
+++ server/tests/seed/seed.js
@@ -17,17 +17,23 @@ const users = [{
 },{
   _id: userTwoId,
   email: 'userTwo@example.com',
-  password: 'userTwoPass'
+  password: 'userTwoPass',
+  tokens: [{
+    access: 'auth',
+    token: jwt.sign({_id: userTwoId, access: 'auth'}, '123abc').toString()
+  }]
 }];
 
 const todos = [{
   _id: new ObjectID(),
-  text: 'First test to do'
+  text: 'First test to do',
+  _creator: userOneId
 }, {
   _id: new ObjectID(),
   text: 'Second test to do',
   completed: true,
-  completedAt: 333
+  completedAt: 333,
+  _creator: userTwoId
 }];
 
 const populateTodos = (done) => {
